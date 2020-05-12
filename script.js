53bf9f1f224c6f879b0616284a3335f8f30fb007
Alessia Alessandra <alessiaalessandra@alessias-mbp-2.home>
2020-05-11 23:04:40 -0400

    I am saving my work. This is a second rendition of my final draft.

--- script.js
+++ script.js
@@ -6,42 +6,48 @@ let jsonDatabase = [
     "picture_url" : "https://cookieandkate.com/images/2017/11/amazing-chocolate-chip-cookies-recipe-2.jpg",
     "description" : "These cookies are a little chubbier than usual chocolate chip cookies. You only need one bowl and you can mix the ingredients by hand!",
     "recipePage" : "cookies.html",
-    "dietary" : ["Vegetarian"]
+    "dietary" : ["Vegetarian"],
+    "viewRecipe" : "Click to view recipe"
   },
   {
     "title" : "Banana Bread",
     "picture_url" : "https://cookieandkate.com/images/2015/11/healthy-banana-bread-1.jpg",
     "description" : "I'm pretty sure almost everyone is making banana bread in quarantine. This banana bread is very dense and moist. For this recipe, I recommend using bananas that are way past the ripe point.",
     "recipePage" : "banana.html",
-    "dietary" : ["Vegetarian", "No added sugar", "Dairy-free", "Whole wheat"]
+    "dietary" : ["Vegetarian", "No added sugar", "Dairy-free", "Whole wheat"],
+    "viewRecipe" : "Click to view recipe"
   },
   {
     "title" : "Chocolate Chip Blondies",
     "picture_url" : "https://cookieandkate.com/images/2017/04/healthy-chocolate-chips-blondies-recipe-1.jpg",
     "description" : "Sometimes brownies are a little bit too heavy. These blondies are lighter but still chocolatey.",
     "recipePage" : "blondies.html",
-    "dietary" : ["Vegetarian"]
+    "dietary" : ["Vegetarian"],
+    "viewRecipe" : "Click to view recipe"
   },
   {
     "title" : "Zucchini Muffins",
     "picture_url" : "https://cookieandkate.com/images/2017/05/healthy-zucchini-muffins-recipe.jpg",
     "description" : "Don't worry, these muffins don't taste at all like zucchini. These are great to have as a healthy snack during the day!",
     "recipePage" : "muffins.html",
-    "dietary" : ["Vegetarian", "No added sugar", "Whole wheat"]
+    "dietary" : ["Vegetarian", "No added sugar", "Whole wheat"],
+    "viewRecipe" : "Click to view recipe"
   },
   {
     "title" : "Anzac Biscuits",
     "picture_url" : "https://www.kingarthurflour.com/sites/default/files/styles/featured_image_2x/public/recipe_legacy/5393-3-large.jpg?itok=IZlJQ8OC",
     "description" : "These biscuits are crunchy on the outside and chewy on the inside. They're a recent discovery of mine that I really enjoy making.",
     "recipePage" : "biscuits.html",
-    "dietary" : ["Vegetarian"]
+    "dietary" : ["Vegetarian"],
+    "viewRecipe" : "Click to view recipe"
   },
   {
     "title" : "Raisin Scones",
     "picture_url" : "https://www.kingarthurflour.com/sites/default/files/styles/featured_image_2x/public/recipe_legacy/44-3-large.jpg?itok=4Ki8IfXV",
     "description" : "Some people love scones, some people hate them. These go really well with any fruit jam you might already have in your pantry.",
     "recipePage" : "scones.html",
-    "dietary" : ["Vegetarian"]
+    "dietary" : ["Vegetarian"],
+    "viewRecipe" : "Click to view recipe"
   }
 ]
 
@@ -70,6 +76,11 @@ function createElement(incomingJSON){
   newTitleLink.appendChild(newImage);
   newRecipeElement.appendChild(newTitleLink);
   //newRecipeElement.appendChild(newImage);
+
+  let newLink = document.createElement("P")
+  newLink.innerHTML = incomingJSON['viewRecipe'];
+  newTitleLink.appendChild(newLink);
+  newRecipeElement.appendChild(newTitleLink);
 
   let newDescription = document.createElement("P")
   newDescription.classList.add("recipeDescription");
